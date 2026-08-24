f9065a4ce72e6104bea0e6ff121fdc45ea15ac86
OldMac <oldmac@example.com>
2026-08-23 21:15:46 -0600

    adds tokyo night color scheme

--- config.h
+++ config.h
@@ -8,15 +8,17 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMonoNL Nerd Font Mono:size=12" };
 static const char dmenufont[]       = "JetBrainsMonoNL Nerd Font Mono:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+/* Tokyo Night colors */
+static const char col_bg[]          = "#1a1b26";
+static const char col_bg_dark[]     = "#16161e";
+static const char col_fg[]          = "#bb9af7";
+static const char col_fg_bright[]   = "#394b70";
+static const char col_blue[]        = "#7aa2f7";
+static const char col_border[]      = "#3b4261";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg              bg            border   */
+	[SchemeNorm] = { col_fg,         col_bg,        col_border },
+	[SchemeSel]  = { col_fg_bright,  col_blue,      col_blue   },
 };
 
 /* tagging */
@@ -59,7 +61,19 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+
+/* Tokyo Night dmenu */
+static const char *dmenucmd[] = {
+	"dmenu_run",
+	"-m", dmenumon,
+	"-fn", dmenufont,
+	"-nb", col_bg_dark,
+	"-nf", col_fg,
+	"-sb", col_blue,
+	"-sf", col_fg_bright,
+	NULL
+};
+
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
@@ -112,9 +126,9 @@ static const Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkTagBar,             0,              Button1,        view,           {0} },
+	{ ClkTagBar,             0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,             MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,             MODKEY,         Button3,        toggletag,       {0} },
 };
 
